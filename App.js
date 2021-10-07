a4c4b672a1aae862565c522dca999b8e7c2f4b06
Henry-Freck <henry.freck@gmail.com>
2021-10-06 21:50:46 -0400

    just added some comments to the App.js page

--- App.js
+++ App.js
@@ -8,18 +8,22 @@ import ProfileScreen from './components/profileScreen';
 import { Ionicons } from '@expo/vector-icons'
 
 
-const TabNavigator = createBottomTabNavigator();
+const TabNavigator = createBottomTabNavigator();     //creating the bottom tab navigator
 
 export default class App extends Component {
+
   render(){
+
     return(
+
     <NavigationContainer>
-      <TabNavigator.Navigator screenOptions = {({route})=>({
+      <TabNavigator.Navigator screenOptions = {({route})=>({       //setting screen options for tab bar navigator
+
         tabBarIcon: ({size, color, focused})=>{
 
           let iconName
 
-          if (route.name === "Match"){
+          if (route.name === "Match"){                            //returning correct ionicon for each page
             iconName = "people-circle-outline"
           }else if (route.name === "Chat"){
             iconName = "chatbubbles-outline"
@@ -27,12 +31,12 @@ export default class App extends Component {
             iconName = "person-circle-outline"
           }
 
-          return <Ionicons name={iconName} size={size} color={color} /> 
+          return <Ionicons name={iconName} size={size} color={color} />           //adding icons  to navbar
 
         },
         tabBarActiveTintColor: "chartreuse", 
         tabBarStyle: {
-          backgroundColor: "black"
+          backgroundColor: "black"                                              //other navbar settings
         }
       })}
       initialRouteName = "Match">
