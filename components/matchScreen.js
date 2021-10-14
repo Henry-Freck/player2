f31f391936ffa5697edae60cc9c9fc3d4744f39b
Henry-Freck <henry.freck@gmail.com>
2021-10-14 16:43:58 -0400

    added buttons and figures to the match page

--- components/matchScreen.js
+++ components/matchScreen.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text, Button} from 'react-native';
 
 export default class MatchScreen extends Component {
+
+yesButton = () => {
+  //enter the code here that runs when the yes button is pressed
+}
+
+noButton = () =>{
+  //code here that gets performed when the no button is pressed
+}
+
   render(){
     return(
     <View style={styles.container}>
-      <Text style={{color:"white",fontSize:30}}>Match</Text>
+      <Text style={{color:"white",fontSize:30}}>The Player Name Will Go Here!</Text>
+      <Text style={{color:"white",fontSize:30}}>Rank</Text>
+      <Text style={{color:"white",fontSize:30}}>The Player Name Will Go Here!</Text>
+
+      <Button title="Yes" onPress={this.yesButton}>Hello there</Button>
+      <Button title="No" onPress={this.yesButton}>Hello there</Button>
+
     </View>
     );
   }
